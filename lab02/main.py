dad178e43bd4019dc2fb1ea8d76121513c149d16
tqhung <80533005+tqhung123456@users.noreply.github.com>
2024-03-01 23:08:22 +0700

    commit

--- lab02/main.py
+++ lab02/main.py
@@ -3,35 +3,21 @@ import time
 
 from adafruit_mqtt import Adafruit_MQTT
 from ai import mask_detection
+from uart import readSerial
 
 mqtt = Adafruit_MQTT()
 time.sleep(5)
+
 client = mqtt.client
 sensor_type = 0
 prev_result = ""
+
 while True:
-    print("Publishing random data...")
+    readSerial(client)
 
-    if sensor_type == 0:
-        print("Temperature...")
-        temp = random.randint(15, 60)
-        client.publish("cambien1", temp)
-        sensor_type = 1
-    elif sensor_type == 1:
-        print("Light...")
-        light = random.randint(0, 500)
-        client.publish("cambien2", light)
-        sensor_type = 2
-    elif sensor_type == 2:
-        print("Humidity...")
-        humi = random.randint(0, 100)
-        client.publish("cambien3", humi)
-        sensor_type = 0
+    # ai_result = mask_detection()
+    # if ai_result != prev_result:
+    #     client.publish("ai", ai_result)
+    #     prev_result = ai_result
 
-    ai_result = mask_detection()
-    print("AI result:", ai_result)
-    if ai_result != prev_result:
-        client.publish("ai", ai_result)
-        prev_result = ai_result
-
-    time.sleep(10)
+    time.sleep(1)
---
+++ lab02/uart.py
@@ -0,0 +1,56 @@
+import serial.tools.list_ports
+
+
+def getPort():
+    ports = serial.tools.list_ports.comports()
+    N = len(ports)
+    commPort = "None"
+    for i in range(0, N):
+        port = ports[i]
+        strPort = str(port)
+
+        # Use this on Windows
+        # if "USB Serial Device" in strPort:
+        #     splitPort = strPort.split(" ")
+        #     commPort = splitPort[0]
+        if "serial port emulator" in strPort:
+            splitPort = strPort.split(" ")
+            commPort = splitPort[0]
+
+        # Use this on WSL2
+        # commPort = "/dev/pts/10"
+    return commPort
+
+
+ser = serial.Serial(port=getPort(), baudrate=115200)
+
+
+def processData(client, data):
+    data = data.replace("!", "")
+    data = data.replace("#", "")
+    splitData = data.split(":")
+    print(splitData)
+    if splitData[1] == "T":
+        client.publish("cambien1", splitData[2])
+    elif splitData[1] == "L":
+        client.publish("cambien2", splitData[2])
+    elif splitData[1] == "H":
+        client.publish("cambien3", splitData[2])
+
+
+mess = ""
+
+
+def readSerial(client):
+    bytesToRead = ser.inWaiting()
+    if bytesToRead > 0:
+        global mess
+        mess = mess + ser.read(bytesToRead).decode("UTF-8")
+        while ("#" in mess) and ("!" in mess):
+            start = mess.find("!")
+            end = mess.find("#")
+            processData(client, mess[start : end + 1])
+            if end == len(mess):
+                mess = ""
+            else:
+                mess = mess[end + 1 :]
